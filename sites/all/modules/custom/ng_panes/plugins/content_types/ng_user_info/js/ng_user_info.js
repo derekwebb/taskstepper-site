fd65cf8045d1386dbe0128e5a7a0ac37d60cc5a7
Derek Webb <derek@collectivecolors.com>
2015-05-22 00:54:30 -0400

    Added angular-resource.min menu-token token and other mods.

--- sites/all/modules/custom/ng_panes/plugins/content_types/ng_user_info/js/ng_user_info.js
+++ sites/all/modules/custom/ng_panes/plugins/content_types/ng_user_info/js/ng_user_info.js
@@ -1,21 +1,59 @@
 (function (angular) {
   'use strict';
-  var userInfoApp = angular.module('UserInfoPane', []);
+  var userInfoApp = angular.module('UserInfoPane', ['ngResource']);
   
-  userInfoApp.controller('UserInfoController', ['$scope', function($scope) {
+  userInfoApp.controller('UserInfoController', ['$scope', 'MenuService', function($scope, MenuService) {
+    
+    // Initialize the controller
     this.init = function() {
       this.userInfo = Drupal.settings.ng_panes.ng_user_info; // Get the user login state
-      
+      this.menuShow = false;
       if (this.userInfo.user_uid != '0') {
         this.userSignedIn = true;
         this.userSignedOut = false;
+        return 'logged in';
       }
       else {
         this.userSignedIn = false;
         this.userSignedOut = true;
+        return 'logged out';
       }
     };
     
+    this.menuToggle = function() {
+      console.log('Toggling');
+      this.menuShow = !this.menuShow;
+    };
+    
+    // Build links from the service call
+    this.buildLinks = function(links) {
+      //console.log(links.tree);
+      var l = links.tree;
+      var set = {};
+      for (var key in l) {
+        if (l.hasOwnProperty(key)) {
+          set[key] = {};
+          //set[key]['path'] = '';
+          //set[key]['title'] = '';
+          set[key]['path'] = l[key].link.path;
+          set[key]['title'] = l[key].link.title;
+        }
+      }
+      this.menu = set; // send to the menu
+    };
+    
     this.state = this.init();
+    if (this.state == 'logged in') {
+      var m = MenuService.get({menu:'main-menu'}, function(m) {
+        // I dont like it but it requires that I ref. the $scope by the name given on the view side "info" ugh
+        $scope.info.buildLinks(m);
+      });
+    }
+    
   }]);
+  
+  // Get the links for the logged in user
+  userInfoApp.factory('MenuService', function($resource) {
+    return $resource("/menu-service/menu/:menu");
+  });
 }(window.angular));
--- sites/all/modules/custom/ng_panes/plugins/content_types/ng_user_info/templates/ng_user_info_pane.tpl.php
+++ sites/all/modules/custom/ng_panes/plugins/content_types/ng_user_info/templates/ng_user_info_pane.tpl.php
@@ -1,16 +1,17 @@
 <div ng-app="UserInfoPane" ng-controller="UserInfoController as info">
   <!-- Logged in user -->
-  <div ng-show="{{info.userSignedIn}}">
+  <div ng-show="info.userSignedIn">
     <a href="/user/{{info.userInfo.user_uid}}" title="View profile of {{info.userInfo.user_name}}">
       <img src="{{info.userInfo.user_image_url}}" alt="Profile picture for user {{info.userInfo.user_name}}">
     </a>
     <a href="/user/{{info.userInfo.user_uid}}" title="View profile of {{info.userInfo.user_name}}">{{info.userInfo.user_name}}</a>
     <!-- ng click here with dropdown of some useful menu items [my account, logout, notifications]-->
-    <a href="#">[+]</a>
+    <a href="#" ng-click="info.menuToggle()">[+]</a>
+    <ul ng-show="info.menuShow"><li ng-repeat="link in info.menu"><a href="{{link.href}}">{{link.title}}</a></li></ul>
   </div>
   
   <!-- Logged out user -->
-  <div ng-show="{{info.userSignedOut}}">
+  <div ng-show="info.userSignedOut">
     <a href="/user/register" class="register">Register</a>
     <a href="/user/login" class="login">Login</a>
   </div>
