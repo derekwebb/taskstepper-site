beff31c3f9a9c7333c45c96ec18bd6db3d9ce3f4
Derek Webb <derek@collectivecolors.com>
2015-05-26 00:06:48 -0400

    Updating some scss and ng_user_panes user_info_pane codes.

--- sites/all/modules/custom/ng_panes/plugins/content_types/ng_user_info/js/ng_user_info.js
+++ sites/all/modules/custom/ng_panes/plugins/content_types/ng_user_info/js/ng_user_info.js
@@ -1,13 +1,19 @@
 (function (angular) {
   'use strict';
+  
+  var appPath = '/sites/all/modules/custom/ng_panes/plugins/content_types/ng_user_info';
   var userInfoApp = angular.module('UserInfoPane', ['ngResource']);
   
-  userInfoApp.controller('UserInfoController', ['$scope', 'MenuService', function($scope, MenuService) {
+  // Controller
+  userInfoApp.controller('UserInfoController', ['$scope', 'MenuService', 'LoginService', function($scope, MenuService, LoginService) {
     
     // Initialize the controller
     this.init = function() {
-      this.userInfo = Drupal.settings.ng_panes.ng_user_info; // Get the user login state
-      this.menuShow = false;
+      this.userInfo     = Drupal.settings.ng_panes.ng_user_info; // Get the user login state
+      this.menuShow     = false;
+      this.showLogin    = false;
+      this.showRegister = false;
+      
       if (this.userInfo.user_uid != '0') {
         this.userSignedIn = true;
         this.userSignedOut = false;
@@ -20,24 +26,55 @@
       }
     };
     
-    this.menuToggle = function() {
-      console.log('Toggling');
+    // Toggle the menu open or closed
+    this.menuToggle = function(event) {
       this.menuShow = !this.menuShow;
+      event.stopPropagation(); // Prevent from triggering window.onclick
+    };
+    // Toggle login
+    this.loginToggle    = function(event) {
+      this.showLogin    = !this.showLogin;
+      this.showRegister = false;
+      event.stopPropagation(); // Prevent from triggering window.onclick
+      event.preventDefault();
+    };
+    // Toggle register
+    this.registerToggle = function(event) {
+      this.showRegister = !this.showRegister;
+      this.showLogin    = false;
+      event.stopPropagation(); // Prevent from triggering window.onclick
+      event.preventDefault();
+    };
+    // Allow the items to close if user clicks elsewhere
+    window.onclick = function(e) {
+      if ($scope.info.menuShow || $scope.info.showRegister || $scope.info.showLogin) {
+        $scope.info.menuShow     = false;
+        $scope.info.showRegister = false;
+        $scope.info.showLogin    = false;
+        $scope.$apply();
+      }
+    };
+    
+    // Prevent menu from closing when clicking on a contained
+    //  link. Less confusion that something is happening this way.
+    this.stopPropagation = function(event) {
+      event.stopPropagation();
     };
     
     // Build links from the service call
     this.buildLinks = function(links) {
-      //console.log(links.tree);
       var l = links.tree;
-      console.log(l);
-      var set = {};
+      var set = [];
       for (var key in l) {
         if (l.hasOwnProperty(key)) {
-          set[key] = {};
-          set[key]['path'] = l[key].link.path;
-          set[key]['title'] = l[key].link.title;
+          set.push({
+            path: '/'+l[key].link.path,
+            title: l[key].link.title,
+            weight: Number(l[key].link.weight)
+          });
         }
       }
+      set = _.sortBy(set, 'weight');
       this.menu = set; // send to the menu
     };
     
@@ -49,10 +86,54 @@
       });
     }
     
+    // Apply last class to menu links
+    this.isLast = function(check) {
+      var cssClass = check ? 'last' : null;
+      return cssClass;
+    };
+    
+    // Call the login factory service
+    this.login = function() {
+      var data = {
+        username: this.username,
+        password: this.password
+      };
+      var login = LoginService.save({}, data, 
+        function(data) {
+          // possibly do stuff with result...
+          document.location.reload(true);
+        },
+        function(reply) {
+          $scope.info.errorMessage = reply.statusText;
+        }  
+      );
+    };   
   }]);
   
   // Get the links for the logged in user
   userInfoApp.factory('MenuService', function($resource) {
     return $resource("/menu-service/menu/:menu");
   });
+  
+  // Login factory service
+  userInfoApp.factory('LoginService', function($resource) {
+    return $resource('/auth-service/user/login');
+  });
+  
+  // Directives for login and register
+  userInfoApp.directive('userLogin', function() {
+    return {
+      restrict: 'A',
+      transclude: false,
+      templateUrl: appPath+'/partials/login.html'
+    };
+  });
+  userInfoApp.directive('userRegister', function() {
+    return {
+      restrict: 'A',
+      transclude: false,
+      templateUrl: appPath+'/partials/register.html'
+    };
+  });
+  
 }(window.angular));
--- sites/all/modules/custom/ng_panes/plugins/content_types/ng_user_info/templates/ng_user_info_pane.tpl.php
+++ sites/all/modules/custom/ng_panes/plugins/content_types/ng_user_info/templates/ng_user_info_pane.tpl.php
@@ -6,13 +6,22 @@
     </a>
     <a href="/user/{{info.userInfo.user_uid}}" title="View profile of {{info.userInfo.user_name}}">{{info.userInfo.user_name}}</a>
     <!-- ng click here with dropdown of some useful menu items [my account, logout, notifications]-->
-    <a href="#" ng-click="info.menuToggle()">[+]</a>
-    <ul ng-show="info.menuShow"><li ng-repeat="link in info.menu"><a href="{{link.path}}">{{link.title}}</a></li></ul>
+    <a ng-click="info.menuToggle($event)">
+      <span ng-show="!info.menuShow" class="tog-on"></span>
+      <span ng-show="info.menuShow" class="tog-off"></span>
+    </a>
+    <ul class="user-menu" ng-show="info.menuShow">
+      <li ng-repeat="link in info.menu" ng-class="info.isLast($last)">
+        <a href="{{link.path}}" ng-click="info.stopPropagation($event)">{{link.title}}</a>
+      </li>
+    </ul>
   </div>
   
   <!-- Logged out user -->
   <div ng-show="info.userSignedOut">
-    <a href="/user/register" class="register">Register</a>
-    <a href="/user/login" class="login">Login</a>
+    <a href="/user/register" class="register" ng-click="info.registerToggle($event)">Register</a>
+    <a href="/user/login" class="login" ng-click="info.loginToggle($event)">Login</a>
+    <div user-login ng-show="info.showLogin"></div>
+    <div user-register ng-show="info.showRegister"></div>
   </div>
 </div>
