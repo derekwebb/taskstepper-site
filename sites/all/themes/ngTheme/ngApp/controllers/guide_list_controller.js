dfff6ff63282f8e663c65d13cda8b04de60c6506
Derek Webb <derek@collectivecolors.com>
2015-06-12 14:28:19 -0400

    Updating guide_list_controller.js to format read more and less links.

--- sites/all/themes/ngTheme/ngApp/controllers/guide_list_controller.js
+++ sites/all/themes/ngTheme/ngApp/controllers/guide_list_controller.js
@@ -5,10 +5,91 @@ var guideListController = angular.module('app')
     'ToolKit',
   function($scope, ViewService, ToolKit) {
   
-  var v = ViewService.get({view:'guides', tid:1}, function(v) {
-    // Ref. the $scope by the name given on the view side "info" ugh
-    console.log(v);
-    $scope.guideList.guides = v;
+  // Storage for which rows need more/less links
+  this.rowIndex = [];
+  
+  // Storage for which rows are showing full vs. short intros
+  this.showIndex = [];
+  
+  this.showFull = false;
+  this.showFullClick = function() {
+    alert('Showfull');
+  };
+  
+  this.trustIntro = function(code) {
+    return $sce.trustAsHtml(code);
+  };
+  
+  // Fetch the guide list view from services
+  var data = ViewService.get({view_endpoint:'guide-service', view_path: 'guide-list'}, function(data) {
+    //console.log(data);
+    
+    // Update/process results 
+    for (var row in data.results) {
+      
+      // Create short intro w/ truncate.js
+      data.results[row].Intro_short = $scope.guideList.getShortIntro(data.results[row], row);
+      
+      // Update intro
+      data.results[row].Introduction = $scope.guideList.updateIntro(data.results[row], row);
+      
+    }
+    $scope.guideList.guides = data.results;
   });
 
+
+  // Add a read less anchor tag at the end of the main intro
+  this.updateIntro = function(row, row_index) {
+    var intro = row['Introduction'].trim();
+    var link = ' <a class="less" ng-click="guideList.showFullClick()">Less</a>';
+    
+    if ($scope.guideList.rowIndex[row_index]) { // only apply Less link if needed
+      var index = intro.length - 1;
+      var tag = [];
+      if (intro.charAt(index) === '>') { // we have a tag at the end
+        index--;
+        do {
+          tag.push(intro.charAt(index));
+          index--;
+        } while (intro.charAt(index) != '/'); // the closing tag
+        index--; // we move the index one more for the "<"
+        tag.reverse(); // Reverse
+        tag = tag.join('');
+      }
+  
+      var inserts = ['div', 'p']; // we insert the Less link here.
+      if (jQuery.inArray(tag, inserts) >= 0) { // insert into the tag
+        intro = intro.substr(0, index) + link + intro.substr(index);
+      }
+      else { // insert at the end of the html
+        intro = intro + link;
+      }
+    }
+
+    return intro; 
+  };
+  
+  
+  // Truncate the long intro into a shorter length blurb
+  this.getShortIntro = function(row, row_index) {
+    var link = ' <a class="more moreish" ng-click="guideList.showFullClick()">Read&nbsp;on</a>';
+    
+    // Truncate if necc.
+    var short_intro = jQuery.truncate(row['Introduction'], {
+      length: 250,
+      words: true,
+      ellipsis: '\u2026' + link
+    });
+
+    var more = jQuery('.more', short_intro); // select more link
+
+    if (more.length) { // do we have a more link
+      $scope.guideList.rowIndex[row_index] = true;
+    }
+    else { // no more link
+      $scope.guideList.rowIndex[row_index] = false;
+    }
+    
+    return short_intro;
+  };
 }]);
