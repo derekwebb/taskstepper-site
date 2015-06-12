2568f41cbab52af6d902becfba2c2ff5b1ce02a7
Derek Webb <derek@collectivecolors.com>
2015-06-12 14:36:31 -0400

    Updating view_service to make use of individual view endpoints. They have count info et.al.

--- sites/all/themes/ngTheme/ngApp/services/view_service.js
+++ sites/all/themes/ngTheme/ngApp/services/view_service.js
@@ -1,6 +1,11 @@
 // View factory service
+
+// This service is now to be used with individual view endpoints
+//  Only the individual endpoints report info about row counts
+//  and accept args.
+
+// view_endpoint is configured in the services admin
+// view_path is the path to the service view (view admin)
 angular.module('app').factory('ViewService', function($resource) {
-  return $resource('/view-service/views/:view', {}, {
-    get: { isArray: true }
-  });
+  return $resource('/:view_endpoint/:view_path', {}, {});
 });
